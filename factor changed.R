488dcef373ed0a70b35b32d7251a33fc47d6b72d
Teeranut Tung <tpraisuwan@gmail.com>
2021-06-14 07:51:59 +0000

    Convert to Factor version 2

--- factor changed.R
+++ factor changed.R
@@ -12,6 +12,13 @@ mtcars %>%
   select(mpg, hp, wt, am) %>%
   mutate (am = factor(am, labels = c("Auto", "Manual")))
 
+#Version2
+mtcars %>% 
+  select(mpg, hp, wt, am) %>%
+  mutate (am = factor(am, 
+                      levels = c(0, 1),
+                      labels = c("Auto", "Manual")))
+
 #filter 
 #am no change
 mtcars %>%
